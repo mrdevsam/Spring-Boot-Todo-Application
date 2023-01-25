dd19ef5b5d8984f3ea7170bd2ff6a50efbdaaf70
mrdevsam <mdshoykotislam05@gmail.com>
2023-01-25 10:09:18 +0000

    first commit

---
+++ src/test/java/com/example/secondcourse/webappdemo/WebAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondcourse.webappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
