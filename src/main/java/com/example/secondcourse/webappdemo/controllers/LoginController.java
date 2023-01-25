9590081cd20df7112521891574ecf82146dca89f
mdshoykotislam05@gmail.com <mdshoykotislam05@gmail.com>
2023-01-26 03:19:48 +0600

    creating login form

--- src/main/java/com/example/secondcourse/webappdemo/controllers/LoginController.java
+++ src/main/java/com/example/secondcourse/webappdemo/controllers/LoginController.java
@@ -2,20 +2,18 @@ package com.example.secondcourse.webappdemo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.ModelMap;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class LoginController {
     
-    // localhost:8080/login?name=
+    // localhost:8080/login
     @RequestMapping("login")
-    public String loadLoginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
-        log.debug("request param is: " + name.toString());
+    public String loadLoginPage() {
         
+        log.debug("inside login controller");
         return "login.html";
     }
 }
