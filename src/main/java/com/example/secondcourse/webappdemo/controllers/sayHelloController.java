c0a69cb865559803d41ec382aefca4e617c76127
mrdevsam <mdshoykotislam05@gmail.com>
2023-01-25 19:18:35 +0000

    adding index page to the view

--- src/main/java/com/example/secondcourse/webappdemo/controllers/sayHelloController.java
+++ src/main/java/com/example/secondcourse/webappdemo/controllers/sayHelloController.java
@@ -2,15 +2,13 @@ package com.example.secondcourse.webappdemo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class sayHelloController {
     
-    //link: http://localhost:8080/say-hello
-    @RequestMapping("say-hello")
-    @ResponseBody
-    public String sayHello() {
-        return "Hello, welcome to the page";
+    //link: http://localhost:8080/
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        return "index.html";
     }
 }
