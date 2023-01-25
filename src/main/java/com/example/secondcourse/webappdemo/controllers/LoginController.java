08e7a1201642a150952deee987642d87ca4ee366
mdshoykotislam05@gmail.com <mdshoykotislam05@gmail.com>
2023-01-26 02:37:03 +0600

    adding query param to the login page

--- src/main/java/com/example/secondcourse/webappdemo/controllers/LoginController.java
+++ src/main/java/com/example/secondcourse/webappdemo/controllers/LoginController.java
@@ -2,12 +2,20 @@ package com.example.secondcourse.webappdemo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.ModelMap;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class LoginController {
     
+    // localhost:8080/login?name=
     @RequestMapping("login")
-    public String loadLoginPage() {
+    public String loadLoginPage(@RequestParam String name, ModelMap model) {
+        model.put("name", name);
+        log.debug("request param is: " + name);
+        
         return "login.html";
     }
 }
