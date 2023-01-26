02509f159d623bc383df8d1bca1354e2dc006e21
mdshoykotislam05@gmail.com <mdshoykotislam05@gmail.com>
2023-01-27 03:07:28 +0600

    adding todo model

---
+++ src/main/java/com/example/secondcourse/webappdemo/model/Todo.java
@@ -0,0 +1,17 @@
+package com.example.secondcourse.webappdemo.model;
+import java.time.LocalDate;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public record Todo{
+    
+    private int id;
+    private String username;
+    private String description;
+    private LocalDate targetDate;
+    private boolean isDone
+    
+}
