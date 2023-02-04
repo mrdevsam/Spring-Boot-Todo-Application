c432f7f11e5d7d45738739ab62af30f376eb28b5
mrdevsam <mdshoykotislam05@gmail.com>
2023-02-04 06:40:42 +0000

    making use of @SessionAttributes with views

--- src/main/java/com/example/secondcourse/webappdemo/controllers/WelcomeController.java
+++ src/main/java/com/example/secondcourse/webappdemo/controllers/WelcomeController.java
@@ -2,6 +2,7 @@ package com.example.secondcourse.webappdemo.controllers;
 
 import org.springframework.stereotype.Controller;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import com.example.secondcourse.webappdemo.services.*;
 
@@ -12,7 +13,8 @@ public class WelcomeController {
     
     // localhost:8080/
     @GetMapping({"", "/", "index"})
-    public String loadWelcomePage() {
+    public String loadWelcomePage(ModelMap model) {
+    	model.put("name", "myusernm");
         log.debug("inside welcome controller");
         return "welcome.html";
     }
